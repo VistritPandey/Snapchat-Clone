b2deedccaf7971ea337b364e0733fff2cdd16f96
vistritpandey <vistritpandey@gmail.com>
2021-01-19 13:50:28 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>First line of code on my Mac</h1>
+      <h1>First line of code</h1>
     </div>
   );
 }
