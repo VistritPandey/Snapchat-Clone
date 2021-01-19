4b17f0c47de26fcd2bb953afec96ff3cfb1d5e24
vistritpandey <vistritpandey@gmail.com>
2021-01-19 14:08:48 +0530

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtZH5V8E40pcJ8T_WjSxt7kcz5GAt_cn4",
+  authDomain: "vistrit-snapchat.firebaseapp.com",
+  projectId: "vistrit-snapchat",
+  storageBucket: "vistrit-snapchat.appspot.com",
+  messagingSenderId: "49962651762",
+  appId: "1:49962651762:web:898becf9eb050561fff2eb",
+  measurementId: "G-NZ64F6NXH0",
+};
