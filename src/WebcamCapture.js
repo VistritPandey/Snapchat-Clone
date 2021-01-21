3ac191129ad16aff272fadcfe35d26a6918c2048
vistritpandey <vistritpandey@gmail.com>
2021-01-21 17:42:16 +0530

    k

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,5 +1,7 @@
-import React, { useRef } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUncheckedOutlined"
+import {IconButton} from "@material-ui/core"
 
 const videoConstraints = {
   height: 400,
@@ -9,16 +11,33 @@ const videoConstraints = {
 
 function WebcamCapture() {
   const webcamRef = useRef(null);
+
+  const [image, setImage] = useState(null)
+
+  const capture = useCallback(() => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImage(imageSrc)
+  },[webcamRef])
+  
   return (
     <div className="webcamCapture">
       <Webcam
         audio={false}
+        mirrored={true}
         height={videoConstraints.height}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
         width={videoConstraints.width}
         videoConstraints={videoConstraints}
       />
+
+      <IconButton>
+      <RadioButtonUncheckedIcon 
+          className="webcamCapture__button"
+          onClick={capture}
+      />
+      </IconButton>
+      <img src={image} alt=""/>
     </div>
   );
 }
