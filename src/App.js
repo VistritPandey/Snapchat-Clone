a0837bdc82d26f45355fbfd8695198f4f16a7fa7
vistritpandey <vistritpandey@gmail.com>
2021-01-22 16:03:26 +0530

    k

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
 
 function App() {
   return (
     <div className="App">
-      <WebcamCapture />
+      <Router>
+        <div className="app__body">
+          <Switch>
+            <Route path="/test">
+              <h1>Test</h1>
+            </Route>
+            <Route exact path="/">
+              <WebcamCapture />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
