f642cefad4388a35b11eb2fb23b59c53402a746a
vistritpandey <vistritpandey@gmail.com>
2021-01-22 17:38:03 +0530

    k

--- src/Preview.js
+++ src/Preview.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import "./Preview.css"
+import {useDispatch, useSelector} from "react-redux"
+import { resetCameraImage, selectCameraImage } from './features/cameraSlice'
+import { useHistory } from 'react-router-dom'
+import {useEffect} from "react"
+import CloseIcon from '@material-ui/icons/Close'
 
 function Preview() {
+    const cameraImage = useSelector(selectCameraImage)
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!cameraImage){
+            history.replace("/")
+        }
+    },[cameraImage, history])
+
+    const closePreview = () => {
+        dispatch(resetCameraImage());
+        
+    }
+
     return (
         <div className="preview">
-            <h1>Preview</h1>
+            <CloseIcon onClick={closePreview} className="preview__close"/>
+            <img src={cameraImage} alt=""/>
         </div>
     )
 }
