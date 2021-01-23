38813e20cf414e1cbf1dee0beda6edde2eba4795
vistritpandey <vistritpandey@gmail.com>
2021-01-23 08:20:43 +0530

    uuid added

--- src/Preview.js
+++ src/Preview.js
@@ -1,33 +1,59 @@
-import React from 'react'
-import "./Preview.css"
-import {useDispatch, useSelector} from "react-redux"
-import { resetCameraImage, selectCameraImage } from './features/cameraSlice'
-import { useHistory } from 'react-router-dom'
-import {useEffect} from "react"
-import CloseIcon from '@material-ui/icons/Close'
+import React from "react";
+import "./Preview.css";
+import { useDispatch, useSelector } from "react-redux";
+import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import CloseIcon from "@material-ui/icons/Close";
+import {
+  AttachFile,
+  Create,
+  Crop,
+  MusicNote,
+  Note,
+  Send,
+  TextFields,
+  Timer,
+} from "@material-ui/icons";
 
 function Preview() {
-    const cameraImage = useSelector(selectCameraImage)
-    const history = useHistory()
-    const dispatch = useDispatch()
+  const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (!cameraImage){
-            history.replace("/")
-        }
-    },[cameraImage, history])
-
-    const closePreview = () => {
-        dispatch(resetCameraImage());
-        
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace("/");
     }
+  }, [cameraImage, history]);
 
-    return (
-        <div className="preview">
-            <CloseIcon onClick={closePreview} className="preview__close"/>
-            <img src={cameraImage} alt=""/>
-        </div>
-    )
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+  };
+
+  const sendPost = () => {
+      
+  }
+
+  return (
+    <div className="preview">
+      <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFields />
+        <Create />
+        <Note />
+        <MusicNote />
+        <AttachFile />
+        <Crop />
+        <Timer />
+      </div>
+      <img src={cameraImage} alt="" />
+      <div onClick={sendPost} className="preview__footer">
+          <h2>Send</h2>
+          <Send fontSize="small" className="preview__sendIcon" />
+      </div>
+    </div>
+  );
 }
 
-export default Preview
+export default Preview;
