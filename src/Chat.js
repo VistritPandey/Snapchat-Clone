bbd6be4df5576683ab86b256b35d52585649eb8f
vistritpandey <vistritpandey@gmail.com>
2021-01-23 09:38:18 +0530

    k

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,17 @@
+import { Avatar } from '@material-ui/core'
+import { StopRounded } from '@material-ui/icons'
 import React from 'react'
 import "./Chat.css"
 
-function Chat({profilePic, username, timestamp, imageUrl, read}) {
+function Chat({id, profilePic, username, timestamp, imageUrl, read}) {
     return (
         <div className="chat">
-            
+            <Avatar className="chat__avatar" src={profilePic} />
+            <div className="chat__info">
+                <h4>{username}</h4>
+                <p>Tap to view.. {new Date(timestamp?.toDate()).toUTCString()}</p>
+            </div>
+            {!read && <StopRounded className="chat__readIcon"/>}
         </div>
     )
 }
