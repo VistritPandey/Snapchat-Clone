c49e045363f6ea125865463d4292cf2c27695dbf
vistritpandey <vistritpandey@gmail.com>
2021-01-23 09:25:50 +0530

    k

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chat({profilePic, username, timestamp, imageUrl, read}) {
+    return (
+        <div className="chat">
+            
+        </div>
+    )
+}
+
+export default Chat
--- src/Chats.js
+++ src/Chats.js
@@ -1,12 +1,56 @@
-import React from 'react'
-import "./Chats.css"
+import { Avatar } from "@material-ui/core";
+import { ChatBubble, Search } from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
+import Chat from "./Chat";
+import "./Chats.css";
+import { db } from "./firebase";
 
 function Chats() {
-    return (
-        <div className="chats">
-            
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
+  return (
+    <div className="chats">
+      <div className="chats__header">
+        <Avatar className="chats__avatar" />
+        <div className="chats__search">
+          <Search />
+          <input placeholder="Friends" type="text" />
         </div>
-    )
+        <ChatBubble className="chats__chatIcon" />
+      </div>
+      <div className="chat__posts">
+        {posts.map(
+          ({
+            id,
+            data: { profilePic, username, timestamp, imageUrl, read },
+          }) => (
+            <Chat
+              key={id}
+              id={id}
+              username={username}
+              timestamp={timestamp}
+              imageUrl={imageUrl}
+              read={read}
+              profilePic={profilePic}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Chats
+export default Chats;
